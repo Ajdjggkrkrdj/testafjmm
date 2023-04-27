5a8b6913be525af6e8f6a0cee61bfe687e1f34bb
Ajdjggkrkrdj <128745808+Ajdjggkrkrdj@users.noreply.github.com>
2023-04-27 18:13:20 -0400

    Add files via upload

---
+++ progress.py
@@ -0,0 +1,19 @@
+from io import BufferedReader, FileIO
+from pathlib import Path
+import time
+
+class Progress(BufferedReader):
+    def __init__(self, filename, read_callback):
+        f = open(filename, "rb")
+        self.filename = Path(filename).name
+        self.__read_callback = read_callback
+        super().__init__(raw=f)
+        self.start = time.time()
+        self.length = Path(filename).stat().st_size
+
+    def read(self, size=None):
+        calc_sz = size
+        if not calc_sz:
+            calc_sz = self.length - self.tell()
+        self.__read_callback(self.tell(), self.length,self.start,self.filename)
+        return super(Progress, self).read(size)
